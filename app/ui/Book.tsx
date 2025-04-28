19971eb6a84e405baa29e731cbd7f449f4c468e9
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-04-28 21:22:03 +0200

    Style book with font and so on

--- app/ui/Book.tsx
+++ app/ui/Book.tsx
@@ -1,10 +1,14 @@
-import { BookData, addBookToLibrary, myLibrary } from '@/app/lib/data';
+import { BookData, addBookToLibrary, myLibrary } from "@/app/lib/data";
 
-export default function Book(bookData: BookData) {
+export default function Book({ bookData }: { bookData: BookData }) {
   return (
-    <div className="bg-[url('/book.png')]">
-      <h1>{bookData.title}</h1>
-      <p>{bookData.author}</p>
+    <div className="flex flex-col justify-center items-center gap-4 font-book px-12 py-20 bg-[url('/book.png')] bg-cover p-6 bg-center w-[400] h-[600] text-center text-black">
+      <h1 className="text-xl">{bookData.title}</h1>
+      <h2 className="text-lg">
+        <span className="text-sm">by</span> {bookData.author}
+      </h2>
+      <span>{bookData.pageCount} Pages</span>
+      <span>has {bookData.isRead ? "" : "not"} been read</span>
     </div>
   );
 }
