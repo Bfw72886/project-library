0cdf1284fc0a9771285941ff01a83715172f79ef
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-04-28 14:39:41 +0200

    Try adding book as background for Book

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Book from '@/app/ui/Book';
 export default function Home() {
   return (
     <div>
-      <Image src="/book.png" alt="book" width={400} height={600} />
       <Book
         id="hehe"
         title="Cthulhus Ruf"
--- app/ui/Book.tsx
+++ app/ui/Book.tsx
@@ -2,9 +2,9 @@ import { BookData, addBookToLibrary, myLibrary } from '@/app/lib/data';
 
 export default function Book(bookData: BookData) {
   return (
-    <>
+    <div className="bg-[url('/book.png')]">
       <h1>{bookData.title}</h1>
       <p>{bookData.author}</p>
-    </>
+    </div>
   );
 }
