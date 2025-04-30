74da6df6ef4a2d89a3331353e15ee2a8c2ae0887
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-04-30 10:49:39 +0200

    Add possibility to change read-status

--- app/ui/Book.tsx
+++ app/ui/Book.tsx
@@ -1,6 +1,14 @@
-import { BookData, addBookToLibrary, myLibrary } from "@/app/lib/data";
+import { BookData, addBookToLibrary, myLibrary } from '@/app/lib/data';
+import { useState } from 'react';
 
 export default function Book({ bookData }: { bookData: BookData }) {
+  const [isRead, setIsRead] = useState(bookData.isRead);
+
+  function bookDataUpdate() {
+    setIsRead(!isRead);
+    bookData.isRead = isRead;
+  }
+
   return (
     <div className="flex flex-col justify-center items-center gap-4 font-book px-12 py-20 bg-[url('/book.png')] bg-cover p-6 bg-center w-[400] h-[600] text-center text-black">
       <h1 className="text-xl">{bookData.title}</h1>
@@ -8,7 +16,12 @@ export default function Book({ bookData }: { bookData: BookData }) {
         <span className="text-sm">by</span> {bookData.author}
       </h2>
       <span>{bookData.pageCount} Pages</span>
-      <span>has {bookData.isRead ? "" : "not"} been read</span>
+      <span
+        className="hover:line-through cursor-pointer"
+        onClick={bookDataUpdate}
+      >
+        has {isRead ? '' : 'not'} been read
+      </span>
     </div>
   );
 }
