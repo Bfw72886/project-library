9500e62c5cd16580ca45754fe460dfbd600c709e
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-04-30 11:24:48 +0200

    Prepare Button to delete book

--- app/ui/Book.tsx
+++ app/ui/Book.tsx
@@ -1,5 +1,6 @@
 import { BookData, addBookToLibrary, myLibrary } from '@/app/lib/data';
 import { useState } from 'react';
+import CloseButton from './CloseButton';
 
 export default function Book({ bookData }: { bookData: BookData }) {
   const [isRead, setIsRead] = useState(bookData.isRead);
@@ -9,8 +10,13 @@ export default function Book({ bookData }: { bookData: BookData }) {
     bookData.isRead = isRead;
   }
 
+  function deleteBook() {
+    alert('deleteBook triggered');
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center gap-4 font-book px-12 py-20 bg-[url('/book.png')] bg-cover p-6 bg-center w-[400] h-[600] text-center text-black">
+    <div className="relative flex flex-col justify-center items-center gap-4 font-book px-12 py-20 bg-[url('/book.png')] bg-cover p-6 bg-center w-[400] h-[600] text-center text-black">
+      <CloseButton className="absolute top-26 right-20" onClick={deleteBook} />
       <h1 className="text-xl">{bookData.title}</h1>
       <h2 className="text-lg">
         <span className="text-sm">by</span> {bookData.author}
